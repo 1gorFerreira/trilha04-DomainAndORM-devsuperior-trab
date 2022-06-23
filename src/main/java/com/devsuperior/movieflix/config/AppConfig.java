a61e17e382fe7192ea1b58848ae1f44d4ae0e6c8
Igor <igorferreiradaniel99@hotmail.com>
2022-06-23 15:48:50 -0300

    Validation: Authentication

--- src/main/java/com/devsuperior/movieflix/config/AppConfig.java
+++ src/main/java/com/devsuperior/movieflix/config/AppConfig.java
@@ -1,8 +1,11 @@
 package com.devsuperior.movieflix.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
@@ -12,4 +15,18 @@ public class AppConfig {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey(jwtSecret);
+		return tokenConverter;
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
 }
