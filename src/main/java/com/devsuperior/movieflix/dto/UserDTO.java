a636fe29d0d60f70ea4a39f019dc887930e2ba96
Igor <igorferreiradaniel99@hotmail.com>
2022-06-23 22:34:05 -0300

    authenticatedUser endpoint

--- src/main/java/com/devsuperior/movieflix/dto/UserDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/UserDTO.java
@@ -9,20 +9,22 @@ public class UserDTO implements Serializable{
 	
 	private Long id;
 	private String name;
+	private String email;
 	
 	public UserDTO() {
 		
 	}
 
-	public UserDTO(Long id, String name) {
+	public UserDTO(Long id, String name, String email) {
 		this.id = id;
 		this.name = name;
+		this.email = email;
 	}
 	
 	public UserDTO(User entity) {
 		id = entity.getId();
 		name = entity.getName();
-		
+		email = entity.getEmail();
 	}
 
 	public Long getId() {
@@ -39,6 +41,14 @@ public class UserDTO implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 }
