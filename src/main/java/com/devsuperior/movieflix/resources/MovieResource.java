c555133fddb8d5511df441f6cd1e1e0b760eed08
Igor <igorferreiradaniel99@hotmail.com>
2022-06-25 16:00:16 -0300

    FindAllPaged(Movies) endpoint

---
+++ src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
@@ -0,0 +1,26 @@
+package com.devsuperior.movieflix.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.services.MovieService;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieResource {
+	
+	@Autowired
+	private MovieService movieService;
+	
+	@GetMapping
+	public ResponseEntity<Page<MovieDTO>> findAllPaged(Pageable pageable){
+		Page<MovieDTO> page = movieService.findAllPaged(pageable);
+		return ResponseEntity.ok().body(page);
+	}
+}
